b852d8b43039324c868053cf06405137eeb0cda8
Sohashubhi <49695304+Sohashubhi@users.noreply.github.com>
2019-08-14 09:40:42 -0400

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainContentComponent } from './main-content/main-content.component';
+import { FooterComponent } from './footer/footer.component';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    MainContentComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AngularFontAwesomeModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
